a9001e888ed22fb980f6f5946d229e65bdfb1484
mparreirinha <miguel.parreirinha@gmail.com>
2025-02-22 17:27:16 +0000

    first commit

---
+++ src/main/java/com/parreirinha/expensetrackerapp/ExpensetrackerappApplication.java
@@ -0,0 +1,13 @@
+package com.parreirinha.expensetrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensetrackerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensetrackerappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parreirinha/expensetrackerapp/ExpensetrackerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parreirinha.expensetrackerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
